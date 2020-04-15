97af2dbd843b9dcf7886ae87c84c04845e628957
hannah <huilee9999@gmail.com>
2020-04-15 06:26:36 -0400

    commit the dice

--- UdemyIOSBootcamp/ViewController.swift
+++ UdemyIOSBootcamp/ViewController.swift
@@ -11,18 +11,15 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var diceImageView1: UIImageView!
-    
     @IBOutlet weak var diceImageView2: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageView1.alpha = 0.5
-        
-        diceImageView2.image = #imageLiteral(resourceName: "DiceFive")
-        diceImageView2.alpha = 0.7
+    }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = [ #imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix") ][Int.random(in: 0...5)]
+        diceImageView2.image = [ #imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix") ][Int.random(in: 0...5)]
     }
 
 }
-
